c2774491e299a9406873286f701138c20b486f20
MellyMel Melnyk <114154525+andystlGit@users.noreply.github.com>
2022-09-28 13:24:59 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,3 +43,7 @@ my_cur.execute("SELECT * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+#Allow the end user to add a fruit to the list
+fruits_selected_1 = streamlit.multiselect("What fruit would you like to add?, list(my_fruit_list.index))
+fruits_to_show = my_fruit_list.loc[fruits_selected_1]
